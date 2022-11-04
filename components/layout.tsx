3bac542cbf4c6e4b9e8a2b880b78f46245ee2a9f
chench53 <chench53.logic@gmail.com>
2022-11-05 00:04:12 +0800

    change to pendora

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,7 @@
+import { ReactNode } from 'react'
 import Footer from './footer'
 
-export default function Layout({ children }) {
+export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
       <div className="min-h-screen" style={{'height': '100vh'}}>
